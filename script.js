9e05a3ce4e56eb40900d00e16093d4650786c0ba
tannazma <97254921+tannazma@users.noreply.github.com>
2023-01-18 17:05:38 +0100

    create function for start game and setBoardHover

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const X_CLASS = "x"
 const CIRCLE_CLASS = "circle"
 const cellElements = document.querySelectorAll(".cell")
+const board = document.getElementById("board")
 
 let circleTurn
-cellElements.forEach(cell => {
-    cell.addEventListener("click", handleClick, { once: true })
-})
+startGame()
+
+function startGame(){
+    circleTurn=false
+    cellElements.forEach(cell => {
+        cell.addEventListener("click", handleClick, { once: true })
+    })
+    setBoardHoverClass()
+}
+
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
     swapTurn()
+    setBoardHoverClass()
 }
 
 
@@ -20,4 +29,13 @@ function placeMark(cell, currentClass) {
 
 function swapTurn() {
     circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    }
+    else { board.classList.add(X_CLASS) }
 }
